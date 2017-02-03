7df7b43971627a90fdf007e76954746ba8065d8d
Harper Frankstone <Harper Frankstone>
2017-02-03 14:32:27 -0600

    trying to get the JSON hooked up

--- javascripts/CarLot.js
+++ javascripts/CarLot.js
@@ -6,8 +6,18 @@ var CarLot = (function () {
   return {
     loadInventory: function (callback) {
       var inventoryLoader = new XMLHttpRequest();
+      inventoryLoader.open('GET', inventory.json);
+      inventoryLoader.onload = function(){
+      	var usedCars = JSON.parse(inventoryLoader.responseText);
+      	console.log("Successful load", usedCars);
+      }
 
+      
       inventoryLoader.addEventListener("load", function () {
+
+      });
+
+      inventoryLoader.addEventListener("error", function(){
 
       });
     }
