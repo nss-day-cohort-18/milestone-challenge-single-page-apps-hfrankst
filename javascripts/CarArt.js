dba3c362d90162def66f5912b2462dded538a2fc
Harper Frankstone <Harper Frankstone>
2017-02-03 15:58:30 -0600

    haven't made much progress, still trying to plan and wrap my head around how to execute this project

---
+++ javascripts/CarArt.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var CarLot = (function (inventory) {
+	//use this IIFE to build the car cards 
+	for (inventory[i] in inventory) {
+		var carCard = 
+		`<article>
+		<img>
+		<p>Description</p>
+		<p>Make</p>
+		<p>Model</p>
+		<p>Year</p>
+		<p>Price</p>
+		</article>`;
+	}
+
+	//build a function to change the border color and thickness
+
+
+})(CarLot || {});
--- javascripts/CarLot.js
+++ javascripts/CarLot.js
@@ -5,21 +5,27 @@ var CarLot = (function () {
 
   return {
     loadInventory: function (callback) {
+      //storing the request to the json file in a newly declared variable
       var inventoryLoader = new XMLHttpRequest();
-      inventoryLoader.open('GET', inventory.json);
+      //when the page loads, run this function to parse the info in the JSON file and make it able to be manipulated
       inventoryLoader.onload = function(){
+      	//store the newly parsed JSON info into a variable. 
+      	//responseText tells the program to look for the text within the JSON rather than the braces and brackets
       	var usedCars = JSON.parse(inventoryLoader.responseText);
       	console.log("Successful load", usedCars);
       }
 
-      
+      //upon the loading of the page, run this function....what do I want the function to do?
       inventoryLoader.addEventListener("load", function () {
 
       });
-
+      //is this saying, "if there is an error loading the page, do this function"? If so, what do I want to do if there's an error
       inventoryLoader.addEventListener("error", function(){
 
       });
+      //The following two lines reach out, open the JSON file, grab the info and then send it into the functions above
+      inventoryLoader.open("GET", "inventory.json");
+      inventoryLoader.send();
     }
   };
 
