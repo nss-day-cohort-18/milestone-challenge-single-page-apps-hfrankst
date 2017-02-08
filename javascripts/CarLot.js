f9968aa300ce75a658335d8711009ba4a694f242
Harper Frankstone <Harper Frankstone>
2017-02-07 19:47:17 -0600

    built the function to create the card, but still not getting info out of inventory object

--- javascripts/CarLot.js
+++ javascripts/CarLot.js
@@ -7,25 +7,28 @@ var CarLot = (function () {
     loadInventory: function (callback) {
       //storing the request to the json file in a newly declared variable
       var inventoryLoader = new XMLHttpRequest();
+
       //when the page loads, run this function to parse the info in the JSON file and make it able to be manipulated
-      inventoryLoader.onload = function(){
-      	//store the newly parsed JSON info into a variable. 
-      	//responseText tells the program to look for the text within the JSON rather than the braces and brackets
-      	var usedCars = JSON.parse(inventoryLoader.responseText);
-      	console.log("Successful load", usedCars);
-      }
-      //upon the loading of the page, run this function..I want the 'product cards' for each car to be made in the div
-      inventoryLoader.addEventListener("load", function () {
+
+      inventoryLoader.addEventListener("load", function putCarsInCards() {
         //this may be the place to call a function from the CarArt file
+        	//responseText tells the program to look for the text within the JSON rather than the braces and brackets
+        var usedCars = JSON.parse(inventoryLoader.responseText);
+        console.log("usedCars", usedCars);
+        console.log("the make of the cars;", usedCars.make);
+        carCard(usedCars);
       });
+
+
       //is this saying, "if there is an error loading the page, do this function"? If so, what do I want to do if there's an error
       inventoryLoader.addEventListener("error", function(){
-
+        alert("Oops, somehow we lost all of your cars!!!");
       });
+
       //The following two lines reach out, open the JSON file, grab the info and then send it into the functions above
       inventoryLoader.open("GET", "inventory.json");
-      inventoryLoader.send();
       console.log("Current inventory is ", inventory);
+      inventoryLoader.send();
     }
   };
 
--- javascripts/main.js
+++ javascripts/main.js
@@ -2,6 +2,32 @@
 function populatePage (inventory) {
   // Loop over the inventory and populate the page
 
+
+    //building the car cards
+      function carCard(usedCars){
+          var carDiv = document.getElementsByClassName('col-sm-4 col-md-6')[0];
+          console.log("testing the waters", carDiv);
+          var oneCar;
+          for(oneCar in usedCars){
+            var carToSell = '';
+            var carStuff = usedCars[oneCar];
+            carToSell += '<div>';
+            //add the make and model of the car
+            carToSell += '<h3>' + usedCars.make + ': ' + usedCars.model + '</h3>';
+            //add the year and price 
+            carToSell += '<h5>' + usedCars.year + '</h5>';
+            carToSell += '<p>' + usedCars.price + '</p>';
+            carToSell += '<p>' + usedCars.description + '</p>';
+            //close the card
+            carToSell += '</div>';
+
+            carDiv.innerHTML += carToSell; 
+            carDiv++;
+          }
+
+
+      };
+
   // Now that the DOM is loaded, establish all the event listeners needed
   CarLot.activateEvents();
 }
