54d5a235dbcd19754094afb8567f4ce51a597d31
Harper Frankstone <Harper Frankstone>
2017-02-21 17:29:03 -0600

    finished getting the input mirroring and the card coloring correctly

--- javascripts/events.js
+++ javascripts/events.js
@@ -7,6 +7,7 @@ var CarLot = (function (oldCarLot) {
 // console.log("test3");
 //the event listeners will go on this page
 	var selectedCarCard = document.getElementsByClassName("col-sm-4");
+   var inputEl = document.getElementById("form-control");//focuses the cursor on the input field when card is clicked.
 oldCarLot.activateEvents = function () {
 	//set event listeners here, and call all functions here 
 	
@@ -16,48 +17,40 @@ oldCarLot.activateEvents = function () {
 
 		for(var i = 0; i < selectedCarCard.length; i++){
 			selectedCarCard[i].addEventListener('click', function (event) {
-				   var inputEl = document.getElementById("form-control");//focuses the cursor on the input field when card is clicked.
-				   inputEl.focus();
-				   var outputTarget = $(event.currentTarget).find('p');
-				   console.log("outputTarget", outputTarget);
-				   event.currentTarget.classList.toggle('clicked');						
-			inputEl.addEventListener('keyup', function(event){
-				var newText = event.currentTarget.value;//storing the value put into the input field here
-				console.log("newText", newText);
-				outputTarget.innerHTML = newText.value;
-				
-			});
+				   var currentCar = event.currentTarget;
+				   oldCarLot.colorChange(currentCar);
+			
 			});//close of the color change event listener function
+
 		}//close the for loop
 
+		oldCarLot.colorChange = function (currentCar) {
+			for(var i = 0; i < selectedCarCard.length; i++){
+				selectedCarCard[i].classList.remove('clicked');
+			}
+				   // console.log("outputTarget", outputTarget);
+				   event.currentTarget.classList.add('clicked');
+				   oldCarLot.mirrorInput(currentCar);	
 
+		};
+		oldCarLot.mirrorInput = function(currentCar){
+		   inputEl.focus();
+		   inputEl.addEventListener('keyup', function(event){
 
-				// console.log("this is the outputTarget", outputTarget);
-					// event.currentTarget.classList.toggle('clicked');
-	// window.onload = function () {
-	// 	var descriptionToBeMirrored = document.getElementsByClassName('mirror');//grabbing the card that has the class clicked 
-	// 		descriptionToBeMirrored.addEventListener('click', function (event) {
-	// 			console.log("descriptionToBeMirrored", event.target);
-	// 		}) 
-	// 	var matchingDescription = document.getElementById("form-control");//grabbing the input field
-	// 		matchingDescription.addEventListener('keyup', function (event) {//adding the listener to the keyup of keys typed in the input field
-	// 		descriptionToBeMirrored.innerHTML = newText;
-				
-	// 	});
-	// }
+				var newText = event.currentTarget.value;//storing the value put into the input field here
+				console.log("currentCar", currentCar);
+				if(currentCar.classList.contains('clicked')){
+				var outputTarget = currentCar.querySelector('p');
 
-	//use child nodes to grab the paragraph rather than the mirror class
-		// console.log(descriptionToBeMirrored);
-		// if()
 				// console.log("newText", newText);
-
-				// .children("p.mirror").text(newText);//I don't have a damn clue how to do this in javascript. I want to do an if statement, saying 'if the div has class of clicked and contains a paragraph with class mirror, then put the newText inside that paragraph'
-				
-			// };
+				// console.log("outputTarget", outputTarget.html());
+				outputTarget.innerHTML = newText;
+			};
+			});
+				inputEl.value = "";
+		};
 	
-
-	
-}//close activate events
+};//close activate events
 	
 	
 
